586c4d55e6c31be6b7ceb1d565ea497fd00ae193
nichols20 <johnnytes10@gmail.com>
2021-09-07 22:34:10 -0400

    installed npm dependencies

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const _ = require("underscore");
 const result = _.contains([1, 2, 3], 4);
 
 console.log(result);
+
+//To state that a npm package is a dev dependency after npm i <package>
+// you append --save-dev Ex: npm i jshint --save-dev
