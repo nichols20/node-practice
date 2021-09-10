5f334dc89c6fd0014f58bf6d8b1c069dbcd08590
nichols20 <johnnytes10@gmail.com>
2021-09-10 12:49:01 -0400

    creating callback functions

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const _ = require("underscore");
+console.log("before");
+getUser(1, (user) => {
+  console.log(user);
+  getRepositories(user.gitHUbUsername, (repos) => {
+    console.log(repos);
+  });
+});
+console.log("after");
 
-const result = _.contains([1, 2, 3], 4);
+/* Three ways to deal with asynchronus code
+ -Callbacks
+ -Promises
+ -Async/Await
+ */
 
-console.log(result);
+//This is an example of a callback, I created a callback method to take the value of id
+//Then above I wrote a function to perform a task based on the callback argument below.
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("reading a user from a database");
 
-//To state that a npm package is a dev dependency after npm i <package>
-// you append --save-dev Ex: npm i jshint --save-dev
+    callback({ id: id, gitHUbUsername: "Andrice" });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    const list = ["repo1", "repo2", "repo3"];
+    callback({ username: username, repos: list });
+  }, 2000);
+}
