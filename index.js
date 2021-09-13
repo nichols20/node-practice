917f5fe96416ddfa5e4fac90e6f2dae27109397a
nichols20 <johnnytes10@gmail.com>
2021-09-13 12:50:56 -0400

    consuming promises

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 console.log("before");
-getUser(1, returnUser);
 console.log("after");
 
-function returnUser(user) {
-  console.log(user);
-  getRepositories(user.gitHUbUsername, displayRepositories);
-}
-
-function displayRepositories(repos) {
-  console.log(repos);
-  getCommits(repo, displayCommits);
-}
-
-function displayCommits(commits) {
-  console.log(commits);
-}
+getUser(1)
+  .then((user) => getRepositories(user.gitHUbUsername))
+  .then((repo) =>
+    getCommits(repo[0])
+      .then((commits) => console.log(commits))
+      .catch((error) => console.log(error.message))
+  );
 
 /* Three ways to deal with asynchronus code
  -Callbacks
@@ -22,19 +15,31 @@ function displayCommits(commits) {
  -Async/Await
  */
 
-//This is an example of a callback, I created a callback method to take the value of id
-//Then above I wrote a function to perform a task based on the callback argument below.
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("reading a user from a database");
+//Replaced the callback function with a promise
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("reading a user from a database");
 
-    callback({ id: id, gitHUbUsername: "Andrice" });
-  }, 2000);
+      resolve({ id: id, gitHUbUsername: "Andrice" });
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    const list = ["repo1", "repo2", "repo3"];
-    callback({ username: username, repos: list });
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const list = ["repo1", "repo2", "repo3"];
+      resolve({ username: username, repos: list });
+    }, 2000);
+  });
+}
+
+function getCommits(repos) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("calling github api");
+      resolve(["commit"]);
+    });
+  });
 }
