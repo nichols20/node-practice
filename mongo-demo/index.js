4893361cb9440310e4bb2727f440e116be15b612
nichols20 <johnnytes10@gmail.com>
2021-09-14 19:33:25 -0400

    saving a document

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -12,16 +12,25 @@ const courseSchema = new mongoose.Schema({
   //when this array is stored each object in the array  will be a key value pair ( index of array: string )
   tags: [String],
   date: { type: Date, default: Date.now },
-  isPublished: true,
+  isPublished: Boolean,
 });
 
-/* The .model method takes two arguments. The first argument is the name of the collection
-The second argument is the schema that is correlated to the defined collection. The result
-of this method is a class */
-const Course = mongoose.model("Course", courseSchema);
-const course = new Course({
-  name: "Node.js Course",
-  author: "Andrice",
-  tags: ["node", "backend"],
-  isPublished: true,
-});
+async function createCourse() {
+  /* The .model method takes two arguments. The first argument is the name of the collection
+ The second argument is the schema that is correlated to the defined collection. The result
+ of this method is a class */
+  const Course = mongoose.model("Course", courseSchema);
+  const course = new Course({
+    name: "Angular Course",
+    author: "Andrice",
+    tags: ["Angular", "frontend"],
+    isPublished: true,
+  });
+
+  /* The .save method is an asynchronouse function that returns a promise 
+    meaning we can await it and get the result later*/
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
