0c8c4bcb19ebdcb6c3a87e477785740f556e0d91
nichols20 <johnnytes10@gmail.com>
2021-09-14 12:44:11 -0400

    creating a mongoose schema

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,3 +4,13 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("connected to mongodb"))
   .catch((error) => console.error(`could not connect to mongodb ${error}`));
+
+/* When declaring a Schema in a mongoose class the first argument is an object*/
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  //when this array is stored each object in the array  will be a key value pair ( index of array: string )
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: true,
+});
